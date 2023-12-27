e20f82f2dce9ac7b9b3b5ad99a9332232dd065e7
Juan <44248472+Kuper-xx@users.noreply.github.com>
2023-12-27 02:37:05 -0300

    Update better_self_modify.py

--- better_self_modify.py
+++ better_self_modify.py
@@ -2,7 +2,7 @@ import random
 import time
 
 #TUTO:
-#is indeed a fascinating example of a self-modifying code concept. It dynamically alters its own source code to switch between two versions of a function (updateCountV1 and updateCountV2). 
+#It is a fascinating example of a self-modifying code concept. It dynamically alters its own source code to switch between two versions of a function (updateCountV1 and updateCountV2). 
 #This is a creative approach to demonstrate how a Python program can read and write its own source file to change its behavior upon subsequent executions.
 #Here's a breakdown of how your script works:
 # * Defining Two Function Versions: Two versions of a function, updateCountV1 and updateCountV2, are defined, each printing a different message.
